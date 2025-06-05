456b8d68fc28d5d7da0cf644418eed593c5b8480
TheSilentIce <srikaryadlapati@gmail.com>
2025-06-05 03:26:48 -0600

    Basic card finished

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <Card />
-      <Card />
+      {/* <Card /> */}
     </>
   );
 }
--- src/UI_Elements/Card.tsx
+++ src/UI_Elements/Card.tsx
@@ -1,3 +1,29 @@
-export default function Card() {
-  return <p> Hello World </p>;
+import philippa from "../assets/philippa.jpg";
+import "./UI_Elements_Styles/card.css";
+
+type CardProps = {
+  name: string;
+  image: string;
+  damage: number;
+  cardClass: string;
+  isHero: boolean;
+};
+
+export default function Card({
+  name,
+  image,
+  damage,
+  cardClass,
+  isHero,
+}: CardProps) {
+  return (
+    <>
+      <div className="bg-emerald-400 w-50 h-75">
+        <div
+          className="w-full h-full bg-cover"
+          style={{ backgroundImage: `url(${philippa})` }}
+        ></div>
+      </div>
+    </>
+  );
 }
